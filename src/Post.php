361bb901d077a408cd733e5aaace944e4ee275cc
Devin Gunay <devingunay@gmail.com>
2019-02-21 19:46:55 -0800

    Stub out archive tests

--- src/Post.php
+++ src/Post.php
@@ -4,7 +4,6 @@ namespace BlogBackend;
 
 use BlogBackend\Exception\NotImplementedException;
 
-
 /**
  * Represents a post.
  */
---
+++ test/ArchiveTest.php
@@ -0,0 +1,33 @@
+<?php declare (strict_types = 1);
+
+namespace BlogBackend\Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class ArchiveTest extends TestCase
+{
+  public function testLoadArchive()
+  {
+    $this->markTestIncomplete('testLoadArchive not written yet.');
+  }
+
+  public function testPostsByRange()
+  {
+    $this->markTestIncomplete('testPostsByRange not written yet.');
+  }
+
+  public function testPostsByTags()
+  {
+    $this->markTestIncomplete('testPostsByTags not written yet.');
+  }
+
+  public function testPublishPost()
+  {
+    $this->markTestIncomplete('testPublishPost not written yet.');
+  }
+
+  public function testGetArchiveByYear()
+  {
+    $this->markTestIncomplete('testGetArchiveByYear not written yet.');
+  }
+}
